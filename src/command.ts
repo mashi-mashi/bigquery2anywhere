f74dbd0997254f81f8924d05cc6442109b271291
mashi-mashi <mashi.07.st@gmail.com>
2021-10-25 23:28:46 +0900

    add credential settings

--- src/command.ts
+++ src/command.ts
@@ -16,9 +16,13 @@ export type SettingType =
       makeSummary?(values: any[]): CommonTypesSlackPayload;
     };
 
+export type CredentialType = {projectId: string; private_key: string; client_email: string};
+
 export type CommandType<QUERY_RESULT extends Record<string, any>, COMPOSE_RESULT extends any[]> = {
   query: string;
   composer: (values: QUERY_RESULT[]) => COMPOSE_RESULT;
-  messenger: (composed: COMPOSE_RESULT, setting: SettingType) => Promise<any>;
+  // Credentialに依存するのびみょう・・・
+  messenger: (composed: COMPOSE_RESULT, setting: SettingType, credentials?: CredentialType) => Promise<any>;
   setting: SettingType;
+  credentials?: CredentialType;
 };
